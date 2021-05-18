0544a41af2be55f1e7652504d5d32b9c3e8ef049
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2021-05-18 18:49:53 -0400

    Add MB C, M, R, T functions.

--- R/mb.R
+++ R/mb.R
@@ -9,5 +9,93 @@ process_mb <- function(uuid, val, fmt, ds,
   prov <- "MB"
 
   # process datasets
-
+  switch(
+    uuid,
+    "0261e07b-85ce-4952-99d1-6e1e9a440291" = {
+      mb_rha <- c(
+        "Interlake-Eastern",
+        "Northern",
+        "Prairie Mountain Health",
+        "Southern Health-Sant\u00E9 Sud", # unicode
+        "Winnipeg"
+      )
+      switch(
+        val,
+        "cases" = {
+          switch(
+            fmt,
+            "hr_cum_current" = {
+              ds$features$attributes %>%
+                dplyr::filter(.data$Area %in% mb_rha) %>%
+                dplyr::select(
+                  .data$RHA,
+                  .data$Total_Cases
+                ) %>%
+                dplyr::rename(
+                  sub_region_1 = .data$RHA,
+                  value = .data$Total_Cases
+                ) %>%
+                helper_cum_current(loc = "hr", val, prov, date_current)
+            },
+            e_fmt()
+          )
+        },
+        "mortality" = {
+          switch(
+            fmt,
+            "hr_cum_current" = {
+              ds$features$attributes %>%
+                dplyr::filter(.data$Area %in% mb_rha) %>%
+                dplyr::select(
+                  .data$RHA,
+                  .data$Deaths
+                ) %>%
+                dplyr::rename(
+                  sub_region_1 = .data$RHA,
+                  value = .data$Deaths
+                ) %>%
+                helper_cum_current(loc = "hr", val, prov, date_current)
+            },
+            e_fmt()
+          )
+        },
+        "recovered" = {
+          switch(
+            fmt,
+            "prov_cum_current" = {
+              ds$features$attributes %>%
+                dplyr::filter(.data$Area == "All") %>%
+                dplyr::select(
+                  .data$Recovered
+                ) %>%
+                dplyr::rename(
+                  value = .data$Recovered
+                ) %>%
+                helper_cum_current(loc = "prov", val, prov, date_current)
+            },
+            e_fmt()
+          )
+        },
+        "testing" = {
+          switch(
+            fmt,
+            "prov_cum_current" = {
+              ds$features$attributes %>%
+                dplyr::filter(.data$Area == "All") %>%
+                dplyr::select(
+                  .data$Total_Tests
+                ) %>%
+                dplyr::rename(
+                  value = .data$Total_Tests
+                ) %>%
+                helper_cum_current(loc = "prov", val, prov, date_current)
+            },
+            e_fmt()
+          )
+        },
+        e_val()
+      )
+    },
+    e_uuid()
+  )
 }
